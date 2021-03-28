5d4a37ee32361030948f10574ef06e1fd8f7cf92
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2021-03-28 11:58:11 +0100

    Kernel now sums following blocks, avoiding calling multiple times

--- main.cu
+++ main.cu
@@ -6,29 +6,29 @@
 #define CUDA_CHECK(status) (assert(status == cudaSuccess))
 #define threads_per_block 1024
 
-__global__ void column_reduce(float * matrix, float * result, int m /* lines */, int n /* columns*/, int num_blocks_per_line) {
+__global__ void column_reduce(float * matrix, float * result, int m /* lines */, int n /* columns*/) {
     extern __shared__ float sdata[];
     unsigned int tid = threadIdx.x;
-    //                                  if last block of the line                   size of last block  else max blockdim
-    unsigned int curr_block_limit = (blockIdx.x % num_blocks_per_line == (num_blocks_per_line - 1)) ? n - (num_blocks_per_line - 1) * threads_per_block : blockDim.x;
-    unsigned int line_block_idx = blockIdx.x % num_blocks_per_line;
-    //                        matrix line                  +        previous blocks in line     + id in block
-    unsigned int i = blockIdx.x / num_blocks_per_line  * n + line_block_idx * threads_per_block + threadIdx.x;
-    
-    if (i < m * n) sdata[tid] = matrix[i]; 
+    unsigned int i = blockIdx.x * n + threadIdx.x; // get to idx th line
+    unsigned int offset = 0;
+
+    // sum all the values from that line to fit in one single block
+    sdata[tid] = 0;
+    while (tid + offset < n) {
+        sdata[tid] += matrix[i + offset];
+        offset += blockDim.x;
+    }
     __syncthreads();
 
-    if (threadIdx.x < curr_block_limit) {
-        for (unsigned int s = 1; tid + s < curr_block_limit; s *= 2) {
-            if (tid % (2*s) == 0) {
-                sdata[tid] += sdata[tid + s];
-            }
-
-            __syncthreads();
+    for (unsigned int s = 1; tid + s < blockDim.x; s *= 2) {
+        if (tid % (2*s) == 0) {
+            sdata[tid] += sdata[tid + s];
         }
 
-        if (tid == 0) {result[blockIdx.x] = sdata[0];}
+        __syncthreads();
     }
+
+    if (tid == 0) {result[blockIdx.x] = sdata[0];}
 
 }
 
@@ -40,10 +40,6 @@ int main(int argc, char * argv[])  {
 
     int m = atoi(argv[1]), n = atoi(argv[2]);
     
-    int num_blocks_per_line = n / threads_per_block + (n % threads_per_block ? 1 : 0);
-    int tot_num_blocks = m * num_blocks_per_line;
-
-    
     unsigned long seed = time(NULL);
     srand(seed); // seed 
     printf("Running with seed %ld\n", seed);
@@ -51,7 +47,7 @@ int main(int argc, char * argv[])  {
     // create row-major matrix m x n
     float * matrix = (float *) malloc(sizeof(float) * m * n); // m x n
     // create array to store result
-    float * result_gpu = (float *) malloc(sizeof(float) * tot_num_blocks); // tot_num_blocks x 1
+    float * result_gpu = (float *) malloc(sizeof(float) * m); // tot_num_blocks x 1
     float * result_cpu = (float *) malloc(sizeof(float) * m); // validation
 
     printf("Populating array \n");
@@ -84,45 +80,14 @@ int main(int argc, char * argv[])  {
     float * matrix_gpu, * device_result, * helper_result = NULL;
 
     CUDA_CHECK(cudaMalloc(&matrix_gpu, sizeof(float) * m * n));
-    CUDA_CHECK(cudaMalloc(&device_result, sizeof(float) * tot_num_blocks));
-    CUDA_CHECK(cudaMemset(device_result, 0, sizeof(float) * tot_num_blocks));
+    CUDA_CHECK(cudaMalloc(&device_result, sizeof(float) * m));
+    CUDA_CHECK(cudaMemset(device_result, 0, sizeof(float) * m));
 
     // move matrix into gpu
     CUDA_CHECK(cudaMemcpy(matrix_gpu, matrix, m * n * sizeof(float), cudaMemcpyHostToDevice));
 
-    printf("Running with %d threads per block, %d num blocks per line, %d tot number blocks, total of %d elements per line\n", threads_per_block, num_blocks_per_line, tot_num_blocks,n);
-
     // call kernel
-    column_reduce<<<tot_num_blocks, threads_per_block, sizeof(float)*threads_per_block>>>(matrix_gpu, device_result, m, n, num_blocks_per_line);
-    
-    if (num_blocks_per_line != 1) {
-        // need to keep calling the kernel until the new values are calculated
-        printf("Allocating memory for extra array\n");
-        
-        CUDA_CHECK(cudaMalloc(&helper_result, sizeof(float) * tot_num_blocks));
-        CUDA_CHECK(cudaMemset(helper_result, 0, sizeof(float) * tot_num_blocks));
-
-
-        while (num_blocks_per_line != 1) {            
-            // swap the values
-            float * tmp;
-            tmp = device_result;
-            device_result = helper_result;
-            helper_result = tmp;
-
-            n = num_blocks_per_line;
-            num_blocks_per_line = n / threads_per_block + (n % threads_per_block ? 1 : 0);
-            tot_num_blocks = num_blocks_per_line * m;
-
-            // wait for last kernel to finish
-            CUDA_CHECK(cudaDeviceSynchronize());
-            printf("Running with %d threads per block, %d num blocks per line, %d tot number blocks, total of %d elements per line\n", threads_per_block, num_blocks_per_line, tot_num_blocks,n);
-            
-            // launch kernel again to reduce further
-            column_reduce<<<tot_num_blocks, threads_per_block, sizeof(float)*threads_per_block>>>(helper_result, device_result, m, n, num_blocks_per_line);
-        }
-
-    }
+    column_reduce<<<m, threads_per_block, sizeof(float)*threads_per_block>>>(matrix_gpu, device_result, m, n);
 
     // Wait for final kernel to finish
     CUDA_CHECK(cudaDeviceSynchronize());
